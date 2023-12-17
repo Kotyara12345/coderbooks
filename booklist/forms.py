a02d51e12c9ecb6d5eafff9c174d1196a69f285d
VictorSproot <89292844844@mail.ru>
2023-12-18 01:14:02 +0300

    Update forms.py

--- booklist/forms.py
+++ booklist/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Comments
+from .models import Comments, Book, CATEGORIES
 
 
 class CommentForm(forms.ModelForm):
@@ -11,3 +11,20 @@ class CommentForm(forms.ModelForm):
             'comment_text': forms.Textarea(attrs={'class': 'form-control'})
         }
 
+
+class BookFilterForm(forms.Form):
+
+    def get_choices():
+
+        choices = [(None, 'Все авторы')]
+        choices += [(value, value) for value in Book.objects.values_list('author_book', flat=True)]
+        return choices
+
+    LANG_CHOICES = [
+        ('', 'Все языки'),
+        (1, 'Русский'),
+        (2, 'Английский')
+    ]
+
+    author_book = forms.ChoiceField(choices=get_choices, required=False,  widget=forms.Select(attrs={'class': 'form-select'}), label='Автор')
+    lang_category = forms.ChoiceField(required=False, choices=LANG_CHOICES,  widget=forms.Select(attrs={'class': 'form-select'}), label='Выберете язык')
