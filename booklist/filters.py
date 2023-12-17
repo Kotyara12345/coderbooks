892aa947d7c96a4689c60fc8dc82bff1258219e1
VictorSproot <89292844844@mail.ru>
2023-12-18 02:53:34 +0300

    Update filters.py

--- booklist/filters.py
+++ booklist/filters.py
@@ -7,8 +7,9 @@ class BookFilter(django_filters.FilterSet):
 
     lang_category = django_filters.ChoiceFilter(choices=CATEGORIES)
     author_book = django_filters.ModelChoiceFilter(queryset='Book.author_book')
+    author_book = django_filters.ModelChoiceFilter(queryset='Book.release_date')
      
     class Meta:
         model = Book
-        fields = ['lang_category', 'author_book']
+        fields = ['lang_category', 'author_book', 'release_date']
 
