78e77c6b5ba1d124c7b66a955fa10618136b4c4a
VictorSproot <89292844844@mail.ru>
2023-12-19 23:02:48 +0300

    Update forms.py

--- booklist/forms.py
+++ booklist/forms.py
@@ -7,13 +7,17 @@ from .models import Book, CATEGORIES
 class BookFilterForm(forms.Form):
 
     def get_choices():
-
         choices = [(None, 'Все авторы')]
         choices += [(value, value) for value in Book.objects.values_list('author_book', flat=True)]
-        return choices
+        uniq_choices = list(set(choices)) 
+        return uniq_choices
 
 
-
+    def get_choices_date():
+        choices_date = [(None, '----------')]
+        choices_date += [(value, value) for value in Book.objects.values_list('release_date', flat=True)]
+        uniq_choices_date = list(set(choices_date))       
+        return uniq_choices_date
 
     
     LANG_CHOICES = [
@@ -24,3 +28,4 @@ class BookFilterForm(forms.Form):
 
     author_book = forms.ChoiceField(choices=get_choices, required=False,  widget=forms.Select(attrs={'class': 'form-select'}), label='Автор')
     lang_category = forms.ChoiceField(required=False, choices=LANG_CHOICES,  widget=forms.Select(attrs={'class': 'form-select'}), label='Выберете язык')
+    release_date = forms.ChoiceField(choices=get_choices_date, required=False,  widget=forms.Select(attrs={'class': 'form-select'}), label='Год издания')
