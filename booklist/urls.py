fbe4f0471c6590b38cbb6a12e06b4ebcc08871e4
VictorSproot <89292844844@mail.ru>
2024-02-09 01:04:58 +0300

    Update urls.py

--- booklist/urls.py
+++ booklist/urls.py
@@ -3,6 +3,10 @@ from .views import book_list
 from .views import category_detail
 from .views import BookDetail
 
+from django.conf.urls import handler404
+from .views import error_404
+
+handler404 = error_404
 
 urlpatterns = [
     path('', book_list, name='book_list_url'),
