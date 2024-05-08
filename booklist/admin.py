08ed5b4e9d1f282f014187298236c07f84779b4e
VictorSproot <89292844844@mail.ru>
2024-05-08 09:14:15 +0300

    Update admin.py

--- booklist/admin.py
+++ booklist/admin.py
@@ -22,7 +22,7 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     prepopulated_fields = {'url': ('name',)}
-    #search_fields = ('name')
+    search_fields = ('name')
 
 @admin.register(Publisher)
 class PublisherAdmin(admin.ModelAdmin):
