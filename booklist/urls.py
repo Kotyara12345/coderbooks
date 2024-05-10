a0df1e022d7a27fea6fa55d81b7b5ab6fc3a5c2a
VictorSproot <89292844844@mail.ru>
2024-05-10 23:49:07 +0300

    Update urls.py

--- booklist/urls.py
+++ booklist/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import book_list
-from .views import category_detail
-from .views import release_detail
+from .views import BookView
+from .views import CategoryView
 from .views import BookDetail
+from .views import ReleaseView
 from . import views
 from django.conf.urls import handler404
 from .views import error_404
@@ -10,12 +10,13 @@ from .views import error_404
 handler404 = error_404
 
 urlpatterns = [
-    path('release/<str:slug>/', release_detail, name='release_books'),
+    path('release/<str:slug>/', views.ReleaseView.as_view(), name='release_books'),
     path('author/<str:slug>/', views.AuthorDetailView.as_view(), name='author_detail'),
     path('publisher/<str:slug>/', views.PublisherDetailView.as_view(), name='publisher_detail'),
-    path('category/<str:slug>/', category_detail, name='category_detail_url'),
-    path('<slug:slug>/', BookDetail.as_view(), name='book_detail_url'),
-    path('', book_list, name='book_list_url'),
+    path('category/<str:slug>/', views.CategoryView.as_view(), name='category_detail_url'),
+    path('<slug:slug>/', views.BookDetail.as_view(), name='book_detail_url'),
+    path('', BookView.as_view(), name='book_list_url'),
 ]
+
 
 
