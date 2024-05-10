4e139a1547594117292cb22cd33930869619d8ef
VictorSproot <89292844844@mail.ru>
2024-05-10 23:38:56 +0300

    Update booklist_tag.py

--- booklist/templatetags/booklist_tag.py
+++ booklist/templatetags/booklist_tag.py
@@ -1,8 +1,22 @@
 from django import template
-from booklist.models import Category
+from booklist.models import Category, Book
 
 register = template.Library()
 
 @register.simple_tag()
 def get_categories():
     return Category.objects.all()
+
+
+
+@register.simple_tag()
+def get_lang(self, request, **kwargs):
+    preferred_language = request.META.get('HTTP_ACCEPT_LANGUAGE')
+    if preferred_language:
+        lang = 'ru' if preferred_language.startswith('ru') else 'en'
+    else:
+        lang = settings.LANGUAGE_CODE
+
+    context = {
+        'lang': lang,
+    }
